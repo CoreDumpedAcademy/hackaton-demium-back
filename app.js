d60e5e42b718ff8f8b0695e3d49cceaa85ff0194
luiscal4a <luiscal4a@gmail.com>
2019-05-18 15:00:08 +0200

    Attempt to fix CORS policy

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const ticketRoutes = require('./routes/ticketRoutes');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', ['Content-Type', '*'])
+    next();
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/hotelRoutes.js
+++ routes/hotelRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const hotelController = require('../controllers/hotelController');
 
 const api = express.Router();
-
+/*
 api.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
   next();
-});
+});*/
 
 api.get('/test', (req, res)=>{
     res.status(200).send({message: "All okay hotel"});
