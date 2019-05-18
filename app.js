93b0182530a74ae892272caa0b50cd7d0a48b087
raular4322 <raular98@hotmail.com>
2019-05-18 13:43:53 +0200

    ticket system

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const hotelRoutes = require('./routes/hotelRoutes');
 const trainRoutes = require('./routes/trainRoutes');
+const ticketRoutes = require('./routes/ticketRoutes');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/hotel', hotelRoutes);
 app.use('/train', trainRoutes);
+app.use('/tickets', ticketRoutes)
 
 
 module.exports = app;
--- models/ticket.js
+++ models/ticket.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const ticketSchema = new Schema({
-  lastName: { type: String, required : true },
+  name: { type: String, required : true },
+  surname: { type : String, required : true},
+  numPeople: { type : Number, required : true },
+  price : { type : Number, required : true },
+  description: { type : String, required : true},
+  isTrain: { type : Boolean, required : true}
 });
 
 module.exports = mongoose.model('ticket', ticketSchema);
