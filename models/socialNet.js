1ff7ff2f1b4e07acfd4ab62dddbc70f57351e890
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2019-05-18 13:07:35 +0200

    social network model

---
+++ models/socialNet.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const socialNetcSchema = new Schema({
+  name: { type: String, required : true },
+  link: { type: String, required: true}
+});
+
+module.exports = mongoose.model('ticket', socialNetSchema);
