f319033798c78f0389ab328932f37717814a5cb1
LordMascachapas <alfredm3007@gmail.com>
2019-05-18 14:39:08 +0200

    get train price request
    origin, destiny and number of travelers is needed to return the price

--- controllers/trainController.js
+++ controllers/trainController.js
@@ -3,9 +3,28 @@ const trains = require('../viajes.json');
 function getPriceFromTo(request, response){
     const from = request.params.from;
     const to = request.params.to;
-    const people = request.params.people;
+    let people = request.params.people;
 
-    return response.status(200).send(trains)
+    let trainsFrom = trains[from];
+    if(!trainsFrom)
+        return response.status(404).send({msg: "origin place not found"});
+
+    let price = trainsFrom[to];
+    if(!price)
+        return response.status(404).send({msg: "destiny place not found"});
+
+    people = parseInt(people, 10);
+    console.log(people);
+    if(isNaN(people) || people < 1)
+        return response.status(404).send({msg: "people parameter most be a number and greater than 0"});
+
+    price *= people;
+    return response.status(200).send({
+        from: from,
+        to: to,
+        people: people,
+        price: price
+    });
 }
 
 module.exports = {
